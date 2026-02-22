61cc9c6840a79093f36a877f513aad810c851030
abedAbulail <167705829+abedAbulail@users.noreply.github.com>
2026-02-23 01:56:47 +0200

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
     setSuccess("");
 
     try {
-      const res = await fetch("http://165.227.11.150/login", {
+      const res = await fetch("/api-proxy/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
