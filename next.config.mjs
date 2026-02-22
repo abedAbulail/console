5c27356c81377907543b03a339e4cca6557bd374
abedAbulail <167705829+abedAbulail@users.noreply.github.com>
2026-02-23 01:55:55 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // أي طلب يبدأ بـ /api-proxy سينتقل لسيرفرك
+        source: "/api-proxy/:path*",
+        destination: "http://165.227.11.150/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
